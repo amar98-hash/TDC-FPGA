ebd0f29b615bc522fb5b008d60af5f317337da70
amar98-hash <dadel.amar98@gmail.com>
2026-06-20 22:37:15 -0400

    added the new files in source.f for script project build

--- build_scripts/sources.f
+++ build_scripts/sources.f
@@ -4,3 +4,9 @@
 ../src/delay_chain.vhd
 ../src/shadow_bank.vhd
 ../src/cdc_logic.vhd
+
+#system design source
+../src/axilite_slave.vhd
+../src/test_led.vhd
+../src/tdc_system.tcl
+../src/tdc_system_wrapper.vhd
